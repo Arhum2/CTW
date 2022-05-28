d970925bcb3c249266293fe847076603092d19fb
Arhum2 <arhumshahzad2003@gmail.com>
2022-05-27 22:41:32 -0400

    Added materials UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <TableContainer component={Paper}>
+    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Volunteer name</TableCell>
+          <TableCell>Senior name</TableCell>
+          <TableCell>Phone number</TableCell>
+          <TableCell>Time</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>Mike</TableCell>
+          <TableCell>linda</TableCell>
+          <TableCell>647 773 5708</TableCell>
+          <TableCell>4:00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </TableContainer>
+  )}
 
-export default App;
+export default App
