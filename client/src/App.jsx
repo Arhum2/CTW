807eb8f27912ca90f45e54b3349978fef2490d38
Arhum2 <arhumshahzad2003@gmail.com>
2022-05-28 20:32:22 -0400

    Resolved table not displaying error

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import MuiTable from "./table";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <MuiTable></MuiTable>
+    </div>
+  );
+}
+
+export default App;
