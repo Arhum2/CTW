70633e192a5d7e54cb9d7da97ac7d701c8ada449
Arhum2 <arhumshahzad2003@gmail.com>
2022-06-11 22:44:29 -0400

    Created connection from flask API to react front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,11 @@ import MuiTable from "./table";
 import React, {useState, useEffect} from "react";
 
 function App() {
+  useEffect(() => {
+   fetch('/call').then(response => response.json().then(data => {
+     console.log(data);
+   })) 
+  }, [])
 
   return (
     <div className="App">
