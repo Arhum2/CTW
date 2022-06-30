7d3f012981d720bfa0e68cabd70b3a248b430613
Arhum2 <arhumshahzad2003@gmail.com>
2022-06-29 22:24:37 -0400

    styling and other minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,38 @@ import TableUi from "./components/table";
 import Form from "./components/form";
 import React, {useState, useEffect} from "react";
 import { Button } from "@mui/material";
+import './index.css'
 
 
 function App() {
 
   // Fetching and handling Flask data
   const [calls, setCalls] = useState([]);
-
   useEffect(() => {
    fetch('/call').then(response => response.json().then(data => {
      setCalls(data.calls)
    })); 
   }, []);
 
+
+
   const [state, setState] = useState(false);
   
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <TableUi calls={calls} />
+    <div className="container">
+      <h1>CTW Call Planner</h1>
+      
+      {/*Table component*/}
+      {calls.length > 0 ? <TableUi calls={calls} /> : 'No Calls'}
 
-      <Button style={{marginTop: 45, margin: '0 auto', display: 'flex'}} variant='contained' onClick={() => setState(!state)}>Add Call</Button>
-
+      {/*Button component*/}      
+      <Button className='btn' style={{
+        
+        marginTop: 45, 
+        margin: '0 auto', 
+        display: 'flex'}} 
+        variant='contained' 
+        onClick={() => setState(!state)}>Add Call</Button>
       { state && <Form/>}
     </div>
   );
--- client/src/components/table.js
+++ client/src/components/table.js
@@ -7,7 +7,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-export const TableUI = ({ calls }) => {
+const TableUI = ({ calls }) => {
     return (
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
